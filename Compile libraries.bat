98fec5c7a3e91ce0f9c9c5f7d0d842c689c0d9b1
Numnzel <ionikshockwave@gmail.com>
2017-07-11 00:19:18 +0200

    Primary files included

---
+++ Compile libraries.bat
@@ -0,0 +1,12 @@
+@echo on
+acc155\acc.exe 0.090\acs\mathlib.acs 0.090\acs\mathlib.o
+pause
+cls
+acc155\acc.exe 0.090\acs\vilelib.acs 0.090\acs\vilelib.o
+pause
+cls
+acc155\acc.exe 0.090\acs\convlib.acs 0.090\acs\convlib.o
+pause
+cls
+acc155\acc.exe 0.090\acs\conversations\conv_aniquiled.acs 0.090\acs\conversations\conv_aniquiled.o
+pause
---
+++ Compile vile script.bat
@@ -0,0 +1,3 @@
+@echo on
+acc155\acc.exe 0.090\acs\vilescr.acs 0.090\acs\vilescr.o
+pause
